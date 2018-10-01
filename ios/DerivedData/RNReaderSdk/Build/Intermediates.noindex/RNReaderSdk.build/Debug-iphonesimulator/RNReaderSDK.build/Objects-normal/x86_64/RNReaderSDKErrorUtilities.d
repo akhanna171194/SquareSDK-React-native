70f9b937fe91c5f4382387122fe2bfffb97a7b6c
Abhishek <abhishek@Abhisheks-MacBook-Pro.local>
2018-10-01 22:07:10 +0530

    SDK

---
+++ ios/DerivedData/RNReaderSdk/Build/Intermediates.noindex/RNReaderSdk.build/Debug-iphonesimulator/RNReaderSDK.build/Objects-normal/x86_64/RNReaderSDKErrorUtilities.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/abhishek/Downloads/react-native-square-reader-sdk-master/ios/RNReaderSDKErrorUtilities.m \
+  /Users/abhishek/Downloads/react-native-square-reader-sdk-master/ios/RNReaderSDKErrorUtilities.h
